b81c52d8418eab6310577c4ab5134c73dccad662
TheJulianJES <TheJulianJES@users.noreply.github.com>
2024-11-26 00:01:38 +0100

    Clean up tests

--- tests/test_linxura.py
+++ tests/test_linxura.py
@@ -13,12 +13,10 @@ zhaquirks.setup()
 
 
 @pytest.mark.parametrize("value", (1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23))
-async def test_Linxura_button(zigpy_device_from_quirk, value):
+async def test_button_ias(zigpy_device_from_quirk, value):
     """Test Linxura button remotes."""
 
     device = zigpy_device_from_quirk(zhaquirks.linxura.button.LinxuraButton)
-
-    # endpoint 1
     cluster = device.endpoints[1].ias_zone
     ias_zone_listener = ClusterListener(cluster)
     ias_zone_status_attr_id = IasZone.AttributeDefs.zone_status.id
@@ -33,181 +31,98 @@ async def test_Linxura_button(zigpy_device_from_quirk, value):
 
 
 @pytest.mark.parametrize("quirk", (zhaquirks.linxura.button.LinxuraButton,))
-async def test_Linxura_button_trigger(zigpy_device_from_quirk, quirk):
+async def test_button_edge_case_request(zigpy_device_from_quirk, quirk):
+    """Test button edge case."""
+
+    device = zigpy_device_from_quirk(quirk)
+    cluster = device.endpoints[1].ias_zone
+    ias_zone_listener = ClusterListener(cluster)
+    ias_zone_status_attr_id = IasZone.AttributeDefs.zone_status.id
+
+    cluster.update_attribute(ias_zone_status_attr_id, 4)
+    # No update should occur for state >= 4
+    assert len(ias_zone_listener.attribute_updates) == 1
+
+
+@pytest.mark.parametrize(
+    "message, button, press_type",
+    [
+        (
+            b"\x18\n\n\x02\x00\x19\x01\x00\xfe\xff0\x01",
+            "button_1",
+            "remote_button_short_press",
+        ),
+        (
+            b"\x18\n\n\x02\x00\x19\x03\x00\xfe\xff0\x01",
+            "button_1",
+            "remote_button_double_press",
+        ),
+        (
+            b"\x18\n\n\x02\x00\x19\x05\x00\xfe\xff0\x01",
+            "button_1",
+            "remote_button_long_press",
+        ),
+        (
+            b"\x18\n\n\x02\x00\x19\x07\x00\xfe\xff0\x01",
+            "button_2",
+            "remote_button_short_press",
+        ),
+        (
+            b"\x18\n\n\x02\x00\x19\x09\x00\xfe\xff0\x01",
+            "button_2",
+            "remote_button_double_press",
+        ),
+        (
+            b"\x18\n\n\x02\x00\x19\x0b\x00\xfe\xff0\x01",
+            "button_2",
+            "remote_button_long_press",
+        ),
+        (
+            b"\x18\n\n\x02\x00\x19\x0d\x00\xfe\xff0\x01",
+            "button_3",
+            "remote_button_short_press",
+        ),
+        (
+            b"\x18\n\n\x02\x00\x19\x0f\x00\xfe\xff0\x01",
+            "button_3",
+            "remote_button_double_press",
+        ),
+        (
+            b"\x18\n\n\x02\x00\x19\x11\x00\xfe\xff0\x01",
+            "button_3",
+            "remote_button_long_press",
+        ),
+        (
+            b"\x18\n\n\x02\x00\x19\x13\x00\xfe\xff0\x01",
+            "button_4",
+            "remote_button_short_press",
+        ),
+        (
+            b"\x18\n\n\x02\x00\x19\x15\x00\xfe\xff0\x01",
+            "button_4",
+            "remote_button_double_press",
+        ),
+        (
+            b"\x18\n\n\x02\x00\x19\x17\x00\xfe\xff0\x01",
+            "button_4",
+            "remote_button_long_press",
+        ),
+    ],
+)
+async def test_button_triggers(zigpy_device_from_quirk, message, button, press_type):
     """Test ZHA_SEND_EVENT case."""
     listener = mock.MagicMock()
     device = zigpy_device_from_quirk(zhaquirks.linxura.button.LinxuraButton)
     cluster = device.endpoints[1].ias_zone
     cluster.add_listener(listener)
 
-    message = b"\x18\n\n\x02\x00\x19\x01\x00\xfe\xff0\x01"
     device.handle_message(260, cluster.cluster_id, 1, 1, message)
     assert listener.zha_send_event.call_count == 1
     assert listener.zha_send_event.call_args == mock.call(
-        "button_1_remote_button_short_press",
+        f"{button}_{press_type}",
         {
-            "button": "button_1",
-            "press_type": "remote_button_short_press",
+            "button": button,
+            "press_type": press_type,
             "command_id": 10,
         },
     )
-
-    message = b"\x18\n\n\x02\x00\x19\x03\x00\xfe\xff0\x01"
-    listener.reset_mock()
-    device.handle_message(260, cluster.cluster_id, 1, 1, message)
-    assert listener.zha_send_event.call_count == 1
-    assert listener.zha_send_event.call_args == mock.call(
-        "button_1_remote_button_double_press",
-        {
-            "button": "button_1",
-            "press_type": "remote_button_double_press",
-            "command_id": 10,
-        },
-    )
-
-    message = b"\x18\n\n\x02\x00\x19\x05\x00\xfe\xff0\x01"
-    listener.reset_mock()
-    device.handle_message(260, cluster.cluster_id, 1, 1, message)
-    assert listener.zha_send_event.call_count == 1
-    assert listener.zha_send_event.call_args == mock.call(
-        "button_1_remote_button_long_press",
-        {
-            "button": "button_1",
-            "press_type": "remote_button_long_press",
-            "command_id": 10,
-        },
-    )
-
-    message = b"\x18\n\n\x02\x00\x19\x07\x00\xfe\xff0\x01"
-    listener.reset_mock()
-    device.handle_message(260, cluster.cluster_id, 1, 1, message)
-    assert listener.zha_send_event.call_count == 1
-    assert listener.zha_send_event.call_args == mock.call(
-        "button_2_remote_button_short_press",
-        {
-            "button": "button_2",
-            "press_type": "remote_button_short_press",
-            "command_id": 10,
-        },
-    )
-
-    message = b"\x18\n\n\x02\x00\x19\x09\x00\xfe\xff0\x01"
-    listener.reset_mock()
-    device.handle_message(260, cluster.cluster_id, 1, 1, message)
-    assert listener.zha_send_event.call_count == 1
-    assert listener.zha_send_event.call_args == mock.call(
-        "button_2_remote_button_double_press",
-        {
-            "button": "button_2",
-            "press_type": "remote_button_double_press",
-            "command_id": 10,
-        },
-    )
-
-    message = b"\x18\n\n\x02\x00\x19\x0b\x00\xfe\xff0\x01"
-    listener.reset_mock()
-    device.handle_message(260, cluster.cluster_id, 1, 1, message)
-    assert listener.zha_send_event.call_count == 1
-    assert listener.zha_send_event.call_args == mock.call(
-        "button_2_remote_button_long_press",
-        {
-            "button": "button_2",
-            "press_type": "remote_button_long_press",
-            "command_id": 10,
-        },
-    )
-
-    message = b"\x18\n\n\x02\x00\x19\x0d\x00\xfe\xff0\x01"
-    listener.reset_mock()
-    device.handle_message(260, cluster.cluster_id, 1, 1, message)
-    assert listener.zha_send_event.call_count == 1
-    assert listener.zha_send_event.call_args == mock.call(
-        "button_3_remote_button_short_press",
-        {
-            "button": "button_3",
-            "press_type": "remote_button_short_press",
-            "command_id": 10,
-        },
-    )
-
-    message = b"\x18\n\n\x02\x00\x19\x0f\x00\xfe\xff0\x01"
-    listener.reset_mock()
-    device.handle_message(260, cluster.cluster_id, 1, 1, message)
-    assert listener.zha_send_event.call_count == 1
-    assert listener.zha_send_event.call_args == mock.call(
-        "button_3_remote_button_double_press",
-        {
-            "button": "button_3",
-            "press_type": "remote_button_double_press",
-            "command_id": 10,
-        },
-    )
-
-    message = b"\x18\n\n\x02\x00\x19\x11\x00\xfe\xff0\x01"
-    listener.reset_mock()
-    device.handle_message(260, cluster.cluster_id, 1, 1, message)
-    assert listener.zha_send_event.call_count == 1
-    assert listener.zha_send_event.call_args == mock.call(
-        "button_3_remote_button_long_press",
-        {
-            "button": "button_3",
-            "press_type": "remote_button_long_press",
-            "command_id": 10,
-        },
-    )
-
-    message = b"\x18\n\n\x02\x00\x19\x13\x00\xfe\xff0\x01"
-    listener.reset_mock()
-    device.handle_message(260, cluster.cluster_id, 1, 1, message)
-    assert listener.zha_send_event.call_count == 1
-    assert listener.zha_send_event.call_args == mock.call(
-        "button_4_remote_button_short_press",
-        {
-            "button": "button_4",
-            "press_type": "remote_button_short_press",
-            "command_id": 10,
-        },
-    )
-
-    message = b"\x18\n\n\x02\x00\x19\x15\x00\xfe\xff0\x01"
-    listener.reset_mock()
-    device.handle_message(260, cluster.cluster_id, 1, 1, message)
-    assert listener.zha_send_event.call_count == 1
-    assert listener.zha_send_event.call_args == mock.call(
-        "button_4_remote_button_double_press",
-        {
-            "button": "button_4",
-            "press_type": "remote_button_double_press",
-            "command_id": 10,
-        },
-    )
-
-    message = b"\x18\n\n\x02\x00\x19\x17\x00\xfe\xff0\x01"
-    listener.reset_mock()
-    device.handle_message(260, cluster.cluster_id, 1, 1, message)
-    assert listener.zha_send_event.call_count == 1
-    assert listener.zha_send_event.call_args == mock.call(
-        "button_4_remote_button_long_press",
-        {
-            "button": "button_4",
-            "press_type": "remote_button_long_press",
-            "command_id": 10,
-        },
-    )
-
-
-@pytest.mark.parametrize("quirk", (zhaquirks.linxura.button.LinxuraButton,))
-async def test_edge_case_request(zigpy_device_from_quirk, quirk):
-    """Test edge case."""
-
-    device = zigpy_device_from_quirk(quirk)
-
-    # endpoint 1
-    cluster = device.endpoints[1].ias_zone
-    ias_zone_listener = ClusterListener(cluster)
-    ias_zone_status_attr_id = IasZone.AttributeDefs.zone_status.id
-
-    cluster.update_attribute(ias_zone_status_attr_id, 4)
-    assert (
-        len(ias_zone_listener.attribute_updates) == 1
-    )  # No update should occur for state >= 4
