f833451e247e36ded273d58a1e5d0f47998cae64
TheJulianJES <TheJulianJES@users.noreply.github.com>
2024-11-26 01:25:15 +0100

    Clean up tests more

--- tests/test_linxura.py
+++ tests/test_linxura.py
@@ -5,43 +5,32 @@ from unittest import mock
 import pytest
 from zigpy.zcl.clusters.security import IasZone
 
-from tests.common import ClusterListener
 import zhaquirks
 import zhaquirks.linxura
 
 zhaquirks.setup()
 
 
-@pytest.mark.parametrize("value", (1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23))
-async def test_button_ias(zigpy_device_from_quirk, value):
+async def test_button_ias(zigpy_device_from_quirk):
     """Test Linxura button remotes."""
 
     device = zigpy_device_from_quirk(zhaquirks.linxura.button.LinxuraButton)
-    cluster = device.endpoints[1].ias_zone
-    ias_zone_listener = ClusterListener(cluster)
     ias_zone_status_attr_id = IasZone.AttributeDefs.zone_status.id
-
-    # button press
-    cluster.update_attribute(ias_zone_status_attr_id, value)
-
-    # ZHA calls update_attribute on the IasZone cluster when it receives a status_change_notification
-    assert len(ias_zone_listener.attribute_updates) == 1
-    assert ias_zone_listener.attribute_updates[0][0] == ias_zone_status_attr_id
-    assert ias_zone_listener.attribute_updates[0][1] == value
-
-
-@pytest.mark.parametrize("quirk", (zhaquirks.linxura.button.LinxuraButton,))
-async def test_button_edge_case_request(zigpy_device_from_quirk, quirk):
-    """Test button edge case."""
-
-    device = zigpy_device_from_quirk(quirk)
     cluster = device.endpoints[1].ias_zone
-    ias_zone_listener = ClusterListener(cluster)
-    ias_zone_status_attr_id = IasZone.AttributeDefs.zone_status.id
+    listener = mock.MagicMock()
+    cluster.add_listener(listener)
 
-    cluster.update_attribute(ias_zone_status_attr_id, 4)
-    # No update should occur for state >= 4
-    assert len(ias_zone_listener.attribute_updates) == 1
+    for i in range(0, 24):
+        # button press
+        cluster.update_attribute(ias_zone_status_attr_id, i)
+
+        # update_attribute on the IasZone cluster is always called
+        assert listener.attribute_updated.call_args[0][0] == ias_zone_status_attr_id
+        assert listener.attribute_updated.call_args[0][1] == i
+
+    # we get 20 events, 4 are discarded as invalid (0, 6, 12, 18)
+    assert listener.attribute_updated.call_count == 24
+    assert listener.zha_send_event.call_count == 20
 
 
 @pytest.mark.parametrize(
@@ -111,9 +100,9 @@ async def test_button_edge_case_request(zigpy_device_from_quirk, quirk):
 )
 async def test_button_triggers(zigpy_device_from_quirk, message, button, press_type):
     """Test ZHA_SEND_EVENT case."""
-    listener = mock.MagicMock()
     device = zigpy_device_from_quirk(zhaquirks.linxura.button.LinxuraButton)
     cluster = device.endpoints[1].ias_zone
+    listener = mock.MagicMock()
     cluster.add_listener(listener)
 
     device.handle_message(260, cluster.cluster_id, 1, 1, message)
